a0249513ab83f2f1cf9848b26714a2ec2fa85e2d
bluestome <zhangxiao918@21cn.com>
2013-08-16 23:16:57 +0800

    2013-08-16 新增客户端缓存类。

---
+++ src/com/bluestome/android/cache/clientcache/FileCache.java
@@ -0,0 +1,52 @@
+
+package com.bluestome.android.cache.clientcache;
+
+import android.content.Context;
+
+import java.io.File;
+
+/**
+ * 文件缓存
+ * 
+ * @ClassName: FileCache
+ * @Description: TODO
+ * @author bluestome
+ * @date 2013-8-16 下午10:50:17
+ */
+public class FileCache {
+
+    private File cacheDir;
+
+    public FileCache(Context context) {
+        // 如果有SD卡则在SD卡中建一个LazyList的目录存放缓存的图片
+        // 没有SD卡就放在系统的缓存目录中
+        if (android.os.Environment.getExternalStorageState().equals(
+                android.os.Environment.MEDIA_MOUNTED))
+            cacheDir = new File(
+                    android.os.Environment.getExternalStorageDirectory(),
+                    "LazyList");
+        else
+            cacheDir = context.getCacheDir();
+        if (!cacheDir.exists())
+            cacheDir.mkdirs();
+    }
+
+    public File getFile(String url) {
+        // 将url的hashCode作为缓存的文件名
+        String filename = String.valueOf(url.hashCode());
+        // Another possible solution
+        // String filename = URLEncoder.encode(url);
+        File f = new File(cacheDir, filename);
+        return f;
+
+    }
+
+    public void clear() {
+        File[] files = cacheDir.listFiles();
+        if (files == null)
+            return;
+        for (File f : files)
+            f.delete();
+    }
+
+}
